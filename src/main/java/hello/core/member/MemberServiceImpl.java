b6a1a166d2ac1ce973af4b612ab982686f5c374d
InHee <ihcho620@gmail.com>
2022-12-08 20:48:50 +0900

    [REFACTOR] MemberServiceImp에서 MemberServiceImpl로 네이밍 리팩토링

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,10 @@
 package hello.core.member;
 
-public class MemberServiceImp implements MemberService {
+public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
-    public MemberServiceImp(MemberRepository memberRepository) {
+    public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
