cb4b84ea699ac0c3265c22518f8bc83f8c0de09c
InHee <ihcho620@gmail.com>
2022-12-15 20:48:25 +0900

    [Test] 싱글톤 패턴을 사용하는 테스트

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingletonTest {
 
     @Test
@@ -23,6 +25,28 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberService1 ≠ memberService2
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+
+//      private으로  생성자를 막아두었다. 컴파일 오류가 발생한다.
+//      new SingletonService();
+//      쓴 방법은 객체를 미리 생성해두는 가장 단순하고 안전한 방법으로 실습진행.
+
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService2 = SingletonService.getInstance();
+        //참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // singletonService1 == singletonService2
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same = 인스턴스가 같은지
+        // equal = equals 비교문
     }
 }
